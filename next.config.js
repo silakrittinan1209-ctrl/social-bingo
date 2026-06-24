7bafa377a2ce655edd7d58fc878c977e41822770
sila <silakrittinan1209@gmail.com>
2026-06-24 19:41:12 +0700

    Add ngrok-skip-browser-warning header to bypass ngrok interstitial page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'ngrok-skip-browser-warning', value: '1' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
